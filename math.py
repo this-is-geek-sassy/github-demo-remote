f2118fb9d8ab359f7db5a0779998e88877f3f0b5
Saswata Mishra <saswata.mishra.cse21@heritage.edu.in>
2021-08-07 10:37:17 +0530

    Added fn headers

--- math.py
+++ math.py
@@ -1,12 +1,16 @@
+# Add implementation
 def add(x,y):
     pass
 
+#subtract implentation
 def subtract(x,y):
     pass
-    
+
+#multiply implentation  
 def multiply(x,y):
     pass
-    
+
+#divide implentation    
 def divide(x,y):
     pass
 
